fb092cffe5834a94af6ab82b30bce73d58a0c5d5
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2025-02-13 15:03:12 -0700

    create unreal project

---
+++ Source/iamaiUnrealPlugin/iamaiUnrealPlugin.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "iamaiUnrealPlugin.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, iamaiUnrealPlugin, "iamaiUnrealPlugin" );
