18f6502cedcc87b15d67736391792483103d7869
Collin Greiss <123586114+CollinGreiss@users.noreply.github.com>
2025-05-02 14:11:06 -0600

    Missed Function

--- Plugins/iamaiUnreal/Source/iamaiUnreal/Private/GGUFModelAsset.cpp
+++ Plugins/iamaiUnreal/Source/iamaiUnreal/Private/GGUFModelAsset.cpp
@@ -17,3 +17,33 @@ void UGGUFModelAsset::BeginDestroy() {
 	}
 
 }
+
+void UGGUFModelAsset::PostRename(UObject* OldOuter, const FName OldName) {
+
+	Super::PostRename(OldOuter, OldName);
+
+	if (FilePath.IsEmpty())
+		return;
+
+	FString OldFullPath = FPaths::ProjectDir() / FilePath;
+
+	FString NewName = GetName();
+	FString Directory = FPaths::GetPath(OldFullPath);
+	FString Extension = FPaths::GetExtension(OldFullPath, true);
+	FString NewFullPath = Directory / (NewName + Extension);
+
+	if (!FPaths::FileExists(OldFullPath)) return;
+
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+
+	if (!PlatformFile.MoveFile(*NewFullPath, *OldFullPath)) {
+
+		UE_LOG(LogTemp, Warning, TEXT("Failed to rename model file from %s to %s"), *OldFullPath, *NewFullPath);
+		return;
+
+	}
+
+	FString RelativeDir = FPaths::GetPath(FilePath);
+	FilePath = FPaths::Combine(RelativeDir, NewName + Extension);
+
+}
--- Plugins/iamaiUnreal/Source/iamaiUnreal/Public/GGUFModelAsset.h
+++ Plugins/iamaiUnreal/Source/iamaiUnreal/Public/GGUFModelAsset.h
@@ -13,6 +13,7 @@ class IAMAIUNREAL_API UGGUFModelAsset : public UObject {
 public:
 
 	virtual void BeginDestroy() override;
+	virtual void PostRename(UObject* OldOuter, const FName OldName) override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GGUF Model")
 	FString FilePath;
